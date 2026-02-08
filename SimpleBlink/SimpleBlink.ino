284d9bb4fdfe2cf270e0964fc5355cc0abfccf7c
André Keßler <andre.kessler91@gmx.de>
2026-02-08 11:14:30 +0100

    Optimized SimpleBlink sketch for more object-orientated implementation

--- SimpleBlink/SimpleBlink.ino
+++ SimpleBlink/SimpleBlink.ino
@@ -3,35 +3,38 @@
 #define RELAY 2
 
 void setup() {
-  Serial.begin(115200);        // Start serial communication at 115200 baud rate
-  pinMode(LED_BUILTIN, OUTPUT); // Set the LED pin as an output
+  Serial.begin(115200);          // Start serial communication at 115200 baud rate
+  pinMode(LED_BUILTIN, OUTPUT);  // Set the LED pin as an output
   pinMode(LED_EXTERN, OUTPUT);
   pinMode(RELAY, OUTPUT);
-  Serial.println("Hello from XIAO ESP32S3!"); // Print message once on start
+  Serial.println("Hello from XIAO ESP32S3!");  // Print message once on start
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, LOW); // builtin LED is powered-on with LOW
-  digitalWrite(LED_EXTERN, HIGH);
-  digitalWrite(RELAY, HIGH);
-  Serial.println("LED ON");
+
+  activateRelay();
   delay(2000);
 
-  digitalWrite(LED_BUILTIN, HIGH);
-  digitalWrite(LED_EXTERN, LOW);
-  digitalWrite(RELAY, LOW);
-  Serial.println("LED OFF");
+  deactivateRelay();
   delay(500);
 
-  digitalWrite(LED_BUILTIN, LOW);
+  activateRelay();
+  delay(500);
+
+  deactivateRelay();
+  delay(500);
+}
+
+void activateRelay() {
+  digitalWrite(LED_BUILTIN, LOW);  // builtin LED is powered-on with LOW
   digitalWrite(LED_EXTERN, HIGH);
   digitalWrite(RELAY, HIGH);
-  Serial.println("LED ON");
-  delay(500);
+  Serial.println("Relay activated!");
+}
 
+void deactivateRelay() {
   digitalWrite(LED_BUILTIN, HIGH);
   digitalWrite(LED_EXTERN, LOW);
   digitalWrite(RELAY, LOW);
-  Serial.println("LED OFF");
-  delay(500);
+  Serial.println("Relay deactivated!");
 }
